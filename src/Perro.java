97eb1b9f7ecabe4eef25ba194f2c5faf0c4cea29
Juan Tibanquiza <juan.tibanquiza@ucuenca.edu.ec>
2026-05-22 20:53:26 -0500

    crear perro y gato

---
+++ src/Perro.java
@@ -0,0 +1,9 @@
+public class Perro {
+public Perro(int id, String nombre, int edad) {
+        super(id, nombre, edad);
+    }
+    @Override
+    public String hacerSonido() {
+        return "Guau guau";
+    }
+}
